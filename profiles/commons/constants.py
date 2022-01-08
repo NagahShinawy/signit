5d113c7d3500e0aed121b9613e7de22aa60b5353
Nagah <E-n.Shinawy@lean.sa>
2022-01-08 09:19:23 +0200

    fixing bug for fullname

--- profiles/commons/constants.py
+++ profiles/commons/constants.py
@@ -4,4 +4,4 @@ constants data
 
 # REGEX
 PWD_REGEX = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,50}$"
-FULLNAME_REGX = r"^[a-zA-Z]{4,30}$"
+FULLNAME_REGX = r"(^[a-zA-Z].*[\s\.]*)$"
